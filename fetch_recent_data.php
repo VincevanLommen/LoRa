3454b00bc05b327977611ea4980043e0e76842e4
VincevanLommen <vincevanlommen@outlook.com>
2025-02-12 10:08:55 +0100

    Final commit of the week

--- fetch_recent_data.php
+++ fetch_recent_data.php
@@ -1,14 +1,14 @@
 <?php
 header('Content-Type: application/json');
 
-//get shared data
+// Includen van gedeelde databaseconfiguratie
 require 'db_config.php';
 
-//pull from data base, last hour
+// Ophalen van gegevens van de afgelopen uur uit de database
 $sql = "SELECT * FROM `LoRa` WHERE Datum >= NOW() - INTERVAL 1 HOUR ORDER BY Datum ASC";
 $result = $conn->query($sql);
 
-//yes
+// Verwerken van het resultaat en omzetten naar JSON
 if ($result->num_rows > 0) {
     $data = [];
     while ($row = $result->fetch_assoc()) {
@@ -19,6 +19,6 @@ if ($result->num_rows > 0) {
     echo json_encode([]);
 }
 
-//close this script
+// Sluiten van de databaseverbinding
 $conn->close();
 ?>
